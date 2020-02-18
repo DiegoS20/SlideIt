c77936d18a50f786fdcb649b640058b2b20de51b
Diego José Saravia Zaldaña <saravia__2000@hotmail.com>
2020-02-18 16:42:35 -0500

    Adding some initial configuration and examples are in production also

--- build/slideit.js
+++ build/slideit.js
@@ -2,10 +2,64 @@
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Hola = function Hola() {
-  _classCallCheck(this, Hola);
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-  console.log('Hola init');
-};
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var instance = new Hola();
+var SlideIt =
+/*#__PURE__*/
+function () {
+  function SlideIt(container_id) {
+    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+    _classCallCheck(this, SlideIt);
+
+    // Declaring properties
+    this.declareInitialOptions();
+    this.container = document.getElementById(container_id); // Validating the container of the sliders exists
+
+    if (this.container === null) {
+      throw new Error("The especified container id is not in the DOM.");
+    }
+
+    var children = this.container.getElementsByClassName('slide');
+
+    if (children.length > 0) {
+      this.createSlides(children);
+    }
+
+    if (Object.entries(options).length > 0) {
+      this.coupleOptions(options);
+    }
+  }
+
+  _createClass(SlideIt, [{
+    key: "declareInitialOptions",
+    value: function declareInitialOptions() {
+      this.options = [{
+        name: "fullPage",
+        value: false,
+        changedByUser: false
+      }];
+    }
+  }, {
+    key: "coupleOptions",
+    value: function coupleOptions(new_options) {
+      var this_ops = this.options.slice();
+      this_ops.forEach(function (op, i) {
+        var new_op = new_options[op.name];
+
+        if (op.value != new_op) {
+          this_ops[i].value = new_op;
+          this_ops[i].changedByUser = true;
+        }
+      });
+      this.options = this_ops;
+    }
+  }, {
+    key: "createSlides",
+    value: function createSlides(children) {}
+  }]);
+
+  return SlideIt;
+}();
--- src/slideit.js
+++ src/slideit.js
@@ -1,1 +1,48 @@
+class SlideIt
+{
+    constructor(container_id, options = {}) {
+        // Declaring properties
+        this.declareInitialOptions();
+        this.container = document.getElementById(container_id);
 
+        // Validating the container of the sliders exists
+        if (this.container === null) {
+            throw new Error("The especified container id is not in the DOM.");
+        }
+
+        const children = this.container.getElementsByClassName('slide');
+        if (children.length > 0) {
+            this.createSlides(children);
+        }
+
+        if (Object.entries(options).length > 0) {
+            this.coupleOptions(options);
+        }
+    }
+
+    declareInitialOptions() {
+        this.options = [
+            {
+                name: "fullPage",
+                value: false,
+                changedByUser: false
+            }
+        ];
+    }
+
+    coupleOptions(new_options) {
+        let this_ops = this.options.slice();
+        this_ops.forEach((op, i) => {
+            const new_op = new_options[op.name];
+            if(op.value != new_op) {
+                this_ops[i].value = new_op;
+                this_ops[i].changedByUser = true;
+            }
+        });
+        this.options = this_ops;
+    }
+
+    createSlides(children) {
+        
+    }
+}
