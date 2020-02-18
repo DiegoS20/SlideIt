c79fdae883e1fe6e40bc63e677d4c1136db445c7
Diego José Saravia Zaldaña <saravia__2000@hotmail.com>
2020-02-18 15:00:32 -0500

    Adding build folder for production

---
+++ build/slideit.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Hola = function Hola() {
+  _classCallCheck(this, Hola);
+
+  console.log('Hola init');
+};
+
+var instance = new Hola();
