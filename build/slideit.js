54a33216aeca647def4f11c321775240496c1bd0
Diego José Saravia Zaldaña <saravia__2000@hotmail.com>
2020-02-21 10:26:14 -0500

    Slides structure created, begining with the movement of them

--- build/slideit.js
+++ build/slideit.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -14,51 +16,140 @@ function () {
 
     _classCallCheck(this, SlideIt);
 
-    // Declaring properties
-    this.declareInitialOptions();
-    this.container = document.getElementById(container_id); // Validating the container of the sliders exists
+    this.declareInitialOptions(); // Declaring properties
+
+    this.container = document.getElementById(container_id);
+    this.modifyCSS(this.container, {
+      position: 'relative',
+      width: '100%',
+      height: '35vh',
+      minHeight: '150px',
+      overflow: 'hidden',
+      transition: "all 0.3s ease",
+      webkitTransition: "all 0.3s ease",
+      mozTransition: "all 0.3s ease",
+      msTransition: "all 0.3s ease",
+      oTransition: "all 0.3s ease"
+    });
 
     if (this.container === null) {
+      // Validating the container of the sliders exists
       throw new Error("The especified container id is not in the DOM.");
     }
 
     var children = this.container.getElementsByClassName('slide');
 
     if (children.length > 0) {
-      this.createSlides(children);
-    }
+      if (Object.keys(options).length > 0) {
+        this.coupleOptions(options);
+      }
 
-    if (Object.entries(options).length > 0) {
-      this.coupleOptions(options);
+      this.createSlides(children);
     }
   }
 
   _createClass(SlideIt, [{
-    key: "declareInitialOptions",
-    value: function declareInitialOptions() {
-      this.options = [{
-        name: "fullPage",
-        value: false,
-        changedByUser: false
-      }];
-    }
-  }, {
     key: "coupleOptions",
-    value: function coupleOptions(new_options) {
-      var this_ops = this.options.slice();
-      this_ops.forEach(function (op, i) {
-        var new_op = new_options[op.name];
+    value: function coupleOptions(newOptions) {
+      var thisOps = this.options.slice();
+      thisOps.forEach(function (op) {
+        var opName = op.name;
+        var newOp = newOptions[opName];
 
-        if (op.value != new_op) {
-          this_ops[i].value = new_op;
-          this_ops[i].changedByUser = true;
+        if (opName in newOptions && op.value != newOp) {
+          op.value = newOp;
+          op.changedByUser = true;
         }
       });
-      this.options = this_ops;
+      this.options = thisOps;
     }
   }, {
     key: "createSlides",
-    value: function createSlides(children) {}
+    value: function createSlides(children) {
+      var slideDisplacement = 1;
+      var _iteratorNormalCompletion = true;
+      var _didIteratorError = false;
+      var _iteratorError = undefined;
+
+      try {
+        for (var _iterator = children[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+          var child = _step.value;
+          var cssStyles = {
+            width: this.options[1].value,
+            height: this.options[2].value,
+            position: 'absolute'
+          };
+
+          if (child !== children[0]) {
+            cssStyles.left = "".concat(100 * slideDisplacement++, "%");
+          }
+
+          this.modifyCSS(child, cssStyles);
+        }
+      } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+            _iterator["return"]();
+          }
+        } finally {
+          if (_didIteratorError) {
+            throw _iteratorError;
+          }
+        }
+      }
+
+      this.createSlidesControls(this.container);
+    }
+  }, {
+    key: "createSlidesControls",
+    value: function createSlidesControls(container) {
+      var controlsValue = this.options[3].value;
+
+      for (var i = 0; i < 2; i++) {
+        var position = i == 0 ? "r" : "l";
+        var control = document.createElement('div');
+        control.classList.add('controls');
+        this.modifyCSS(control, _defineProperty({
+          width: '5%',
+          height: '100%',
+          top: 0,
+          position: 'absolute'
+        }, position === 'r' ? "right" : "left", 0));
+        container.appendChild(control);
+      }
+    }
+  }, {
+    key: "modifyCSS",
+    value: function modifyCSS(element, css) {
+      for (var key in css) {
+        var value = css[key];
+        element.style[key] = value;
+      }
+    }
+  }, {
+    key: "declareInitialOptions",
+    value: function declareInitialOptions() {
+      this.options = [{
+        name: "fullScreenSize",
+        value: false,
+        changedByUser: false
+      }, {
+        name: "slidesWidth",
+        value: "100%",
+        changedByUser: false
+      }, {
+        name: "slidesHeight",
+        value: "100%",
+        changedByUser: false
+      }, {
+        name: "slidesControls",
+        value: 'default',
+        changedByUser: false
+      }];
+    }
   }]);
 
   return SlideIt;
--- src/slideit.js
+++ src/slideit.js
@@ -1,48 +1,110 @@
 class SlideIt
 {
     constructor(container_id, options = {}) {
-        // Declaring properties
-        this.declareInitialOptions();
+        this.declareInitialOptions(); // Declaring properties
         this.container = document.getElementById(container_id);
+        this.modifyCSS(this.container, {
+            position: 'relative',
+            width: '100%',
+            height: '35vh',
+            minHeight: '150px',
+            overflow: 'hidden',
+            transition: "all 0.3s ease",
+            webkitTransition: "all 0.3s ease",
+            mozTransition: "all 0.3s ease",
+            msTransition: "all 0.3s ease",
+            oTransition: "all 0.3s ease",
+        });
 
-        // Validating the container of the sliders exists
-        if (this.container === null) {
+        if (this.container === null) { // Validating the container of the sliders exists
             throw new Error("The especified container id is not in the DOM.");
         }
 
         const children = this.container.getElementsByClassName('slide');
         if (children.length > 0) {
+            if (Object.keys(options).length > 0) {
+                this.coupleOptions(options);
+            }
             this.createSlides(children);
         }
+    }
 
-        if (Object.entries(options).length > 0) {
-            this.coupleOptions(options);
+    coupleOptions(newOptions) {
+        let thisOps = this.options.slice();
+        thisOps.forEach((op) => {
+            const opName = op.name;
+            const newOp = newOptions[opName];
+            
+            if (opName in newOptions && op.value != newOp) {
+                op.value = newOp;
+                op.changedByUser = true;
+            }
+        });
+        this.options = thisOps;
+    }
+
+    createSlides(children) {
+        let slideDisplacement = 1;
+        for (const child of children) {
+            let cssStyles = {
+                width: this.options[1].value,
+                height: this.options[2].value,
+                position: 'absolute'
+            };
+            if (child !== children[0]) {
+                cssStyles.left = `${100 * slideDisplacement++}%`;
+            }
+            this.modifyCSS(child, cssStyles);
+        }
+        this.createSlidesControls(this.container);
+    }
+
+    createSlidesControls(container) {
+        const controlsValue = this.options[3].value;
+        for(let i = 0; i < 2; i++) {
+            const position = i == 0 ? "r" : "l";
+            let control = document.createElement('div');
+            control.classList.add('controls');
+            this.modifyCSS(control, {
+                width: '5%',
+                height: '100%',
+                top: 0,
+                position: 'absolute',
+                [position === 'r' ? "right" : "left"]: 0,
+            });
+            container.appendChild(control);
+        }
+    }
+
+    modifyCSS(element, css) {
+        for(const key in css) {
+            const value = css[key];
+            element.style[key] = value;
         }
     }
 
     declareInitialOptions() {
         this.options = [
             {
-                name: "fullPage",
+                name: "fullScreenSize",
                 value: false,
                 changedByUser: false
+            },
+            {
+                name: "slidesWidth",
+                value: "100%",
+                changedByUser: false
+            },
+            {
+                name: "slidesHeight",
+                value: "100%",
+                changedByUser: false
+            },
+            {
+                name: "slidesControls",
+                value: 'default',
+                changedByUser: false,
             }
         ];
-    }
-
-    coupleOptions(new_options) {
-        let this_ops = this.options.slice();
-        this_ops.forEach((op, i) => {
-            const new_op = new_options[op.name];
-            if(op.value != new_op) {
-                this_ops[i].value = new_op;
-                this_ops[i].changedByUser = true;
-            }
-        });
-        this.options = this_ops;
-    }
-
-    createSlides(children) {
-        
     }
 }
